736ab18ce4952139a1c3fcaac18883f109c11b55
Julien Bourdeau <julien@sigerr.org>
2015-04-15 17:39:49 +0200

    Use old vim config (from Ian)

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -29,3 +29,6 @@ if [[ `uname` == 'Darwin' ]]; then
   link "$dotfiles/sublime/Packages/User/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
   link "$dotfiles/sublime/Packages/User/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
 fi
+
+# Vim config
+link "$dotfiles/vim" "$HOME/.vim"
