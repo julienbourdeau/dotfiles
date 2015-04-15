4e2934b790d68bf178cc18c11c337ac7db03fc86
Julien Bourdeau <julien@sigerr.org>
2015-04-15 16:25:41 +0200

    Replace boostrap.sh with symlink script

---
+++ symlink-dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dotfiles="$HOME/Projects/dotfiles3"
+
+if [[ -d "$dotfiles" ]]; then
+  echo "Symlinking dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+link() {
+  from="$1"
+  to="$2"
+  echo "Linking '$from' to '$to'"
+  rm -f "$to"
+  ln -s "$from" "$to"
+}
+
+# Link dotfiles
+for location in $(find home -name '*.sh'); do
+  file="${location##*/}"
+  file="${file%.sh}"
+  link "$dotfiles/$location" "$HOME/.$file"
+done
+
+# Sublime
+if [[ `uname` == 'Darwin' ]]; then
+  link "$dotfiles/sublime/Packages/User/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+  link "$dotfiles/sublime/Packages/User/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
+fi
