541e115171978b45f485f4a00f0b28d54acce666
Julien Bourdeau <julien@sigerr.org>
2015-10-03 18:31:50 +0200

    Use Brew Cask to install GUI software

---
+++ etc/brew-cask.sh
@@ -0,0 +1,33 @@
+# Install Brew Cask
+# https://github.com/caskroom/homebrew-cask
+
+brew install caskroom/cask/brew-cask
+
+# Install dev tools
+
+#brew cask install sublime-text3
+brew cask install iterm2
+brew cask install sourcetree
+brew cask install gitter
+brew cask install structurer
+brew cask install virtualbox
+
+
+# Install other softwares
+
+brew cask install cyberduck
+brew cask install firefox
+brew cask install google-chrome
+brew cask install divvy
+brew cask install vlc
+brew cask install transmission
+brew cask install handbrake
+brew cask install spotify
+brew cask install sonos
+brew cask install dropbox
+brew cask install google-drive
+brew cask install chromecast
+brew cask install teamviewer
+
+
+
