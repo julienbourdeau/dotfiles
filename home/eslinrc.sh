823b6dad0bd83ee5af833706de067868de6ad19b
Julien Bourdeau <julien@sigerr.org>
2015-12-11 16:21:49 +0100

    Add .eslintrc config file

---
+++ home/eslinrc.sh
@@ -0,0 +1,19 @@
+{
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true
+    },
+    "rules": {
+        "semi": 0,
+        "eqeqeq": 1,
+        "new-cap": 1,
+        "no-undef": 2,
+        "no-unused-vars": 1,
+        "max-depth": [1, 3],
+        "no-loop-func": 1
+    }
+}
