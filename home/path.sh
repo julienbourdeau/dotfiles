da4fdd18261cbe08f6a1294477ea774d19aa6735
Julien Bourdeau <julien@sigerr.org>
2016-02-14 12:15:02 +0100

    PATH: Add some GNU utils to path

--- home/path.sh
+++ home/path.sh
@@ -1,2 +1,15 @@
 # Homebrew bin directories
 export PATH="~/.bin:/usr/local/bin:/usr/local/sbin:$PATH";
+
+# Use GNU utils
+PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+
+###############
+#   MANPATH   #
+###############
+
+# Manpages for GNU utils
+MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
