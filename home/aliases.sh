29389deed490d13376f1c984b4c9b6323fcb2ad2
Julien Bourdeau <julien@sigerr.org>
2017-02-10 15:56:16 +0100

    Aliases: Add `ifactive` alias Show active network interfaces

--- home/aliases.sh
+++ home/aliases.sh
@@ -55,6 +55,9 @@ alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew c
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+# Show active network interfaces
+alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
+
 
 # Flush Directory Service cache
 alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
