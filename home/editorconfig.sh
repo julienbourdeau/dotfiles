c77242e0d4167c9005d48e77ebe7bc6c635efa60
Julien Bourdeau <julien@sigerr.org>
2017-02-12 12:00:35 +0100

    .editorconfig: Use 2 spaces for indent (except php)

--- home/editorconfig.sh
+++ home/editorconfig.sh
@@ -2,7 +2,14 @@ root = true
 
 [*]
 charset = utf-8
-indent_style = tab
-end_of_line = lf
+indent_style = space
 insert_final_newline = true
 trim_trailing_whitespace = true
+indent_size = 2
+end_of_line = lf
+
+[*.php]
+indent_size = 4
+
+[*.md]
+trim_trailing_whitespace = false
