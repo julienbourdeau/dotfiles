86afe7d038b08d6f7787cb1c997da9e0ed8c8c7b
Julien Bourdeau <julien@julienbourdeau.com>
2022-10-13 20:14:17 +0200

    git.io is dead

--- home/bash/functions.sh
+++ home/bash/functions.sh
@@ -62,15 +62,6 @@ function dataurl() {
 	echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
 }
 
-# Create a git.io short URL
-function gitio() {
-	if [ -z "${1}" -o -z "${2}" ]; then
-		echo "Usage: \`gitio slug url\`";
-		return 1;
-	fi;
-	curl -i https://git.io/ -F "url=${2}" -F "code=${1}";
-}
-
 # git rebase branch
 function gupdate() {
 	name=$1
