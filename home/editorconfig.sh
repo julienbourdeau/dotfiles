93dc0fdc32139db98e68779c67f1bfb9274814d9
Julien Bourdeau <julien@julienbourdeau.com>
2022-11-12 09:28:26 +0100

    Simplify editorconfig. Most of it should be site in my IntelliJ

--- home/editorconfig.sh
+++ home/editorconfig.sh
@@ -2,20 +2,12 @@ root = true
 
 [*]
 charset = utf-8
-indent_style = space
 insert_final_newline = true
 trim_trailing_whitespace = true
-indent_size = 2
 end_of_line = lf
-
-[*.php]
-indent_size = 4
 
 [*.blade.php]
 indent_size = 2
-
-[*.{json,yml,yaml}]
-indent_size = 4
 
 [*.md]
 trim_trailing_whitespace = false
