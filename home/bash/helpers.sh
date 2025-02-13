0ec7556dd974645b252f4bed0ca74431f922babc
Julien Bourdeau <julien@julienbourdeau.com>
2025-02-13 06:43:52 +0100

    Use %b in e_* helper functions
    Updated all printf format strings to use %b instead of %s to ensure proper handling of escape sequences and formatted output.

--- home/bash/helpers.sh
+++ home/bash/helpers.sh
@@ -23,19 +23,19 @@ function e_header() {
 	printf "\n\n${yellow}==========  %s  ==========${reset}\n" "$@"
 }
 function e_arrow() {
-	printf "➜ %s\n" "$@"
+	printf "➜ %b\n" "$@"
 }
 function e_success() {
-	printf "${green}✔ %s${reset}\n" "$@"
+	printf "${green}✔ %b${reset}\n" "$@"
 }
 function e_error() {
-	printf "${red}✖ %s${reset}\n" "$@"
+	printf "${red}✖ %b${reset}\n" "$@"
 }
 function e_warning() {
-	printf "${purple}➜ %s${reset}\n" "$@"
+	printf "${purple}➜ %b${reset}\n" "$@"
 }
 function e_note() {
-	printf "${blue}Note: %s${reset}\n" "$@"
+	printf "${blue}Note: %b${reset}\n" "$@"
 }
 
 export -f e_header e_arrow e_success e_error e_warning e_note
