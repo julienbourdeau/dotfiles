83d930ffc8fed943dea428deaade5d409398fedd
Julien Bourdeau <julien@julienbourdeau.com>
2025-05-20 12:07:01 +0200

    lago helpers

--- home/bash/lago.sh
+++ home/bash/lago.sh
@@ -1,5 +1,7 @@
 alias lago="docker compose -f $LAGO_PATH/docker-compose.dev.yml -f $LAGO_LICENSE_PATH/docker-compose.dev.yml"
-alias lagup="lago up -d db redis traefik mailhog clickhouse license && lago up front api api-worker api-clock pdf"
+alias lagup-deamon="lago up -d db redis traefik mailhog clickhouse license"
+alias lagup-api="lagup-deamon && lago up api"
+alias lagup="lagup-deamon && lago up front api api-worker api-clock pdf"
 alias lagr="lago exec api bin/rails"
 alias lagapi="lago exec api"
 alias lago_stop="lago stop db redis traefik mailhog clickhouse redpanda license front api api-worker api-clock pdf api-events-worker api-pdfs-worker api-billing-worker api-clock-worker api-webhook-worker"
